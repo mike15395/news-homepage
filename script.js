68bbf6be4ecd71cc17982cce65671ed0ff374dc6
mike15395 <mikhildesai@gmail.com>
2025-03-06 18:46:54 +0530

    mobile-menu-remaining-rest done

---
+++ script.js
@@ -0,0 +1,11 @@
+let mobileMenuIcon = document.querySelector(".mobile-menu-image");
+let mobileMenu = document.querySelector(".sidebar-mobile-menu");
+
+let mobileClose = document.querySelector(".mobile-menu-close");
+// mobileMenuIcon.addEventListener("click", function () {
+//   mobileMenu.style.display = "block";
+// });
+
+// mobileClose.addEventListener("click", function () {
+//   mobileMenu.style.display = "none";
+// });
